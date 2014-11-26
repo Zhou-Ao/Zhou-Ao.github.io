ede08dd8bfded44a0de67ed883d4fca311ceaadd
Zhou Ao <zhouao8@gmail.com>
2014-11-27 00:51:06 +0800

    update

--- js/height.js
+++ js/height.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-		// some code..
-	} else {
-		$("#content-inner").css(
-			"padding-bottom",
-			function(index, value) {
-				var h1 = $(document).height();
-				var h2 = $("#sidebar").height();
-				if (h1 - h2 > 100)
-					return (h1 - h2);
-				else return value;
-			}
-		);
 
-		$("#copyright").css({
-			position: 'absolute',
-			bottom: '5px',
-			left: '0px'
-		});;
-	}
+	$("#content-inner").css(
+		"padding-bottom",
+		function(index, value) {
+			var h1 = $(document).height();
+			var h2 = $("#sidebar").height();
+			if (h1 - h2 > 100)
+				return (h1 - h2);
+			else return value;
+		}
+	);
+
+	$("#copyright").css({
+		position: 'absolute',
+		bottom: '5px',
+		left: '0px'
+	});;
+
 });
