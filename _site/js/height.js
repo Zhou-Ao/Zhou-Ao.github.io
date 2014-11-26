c5857a8b9ac5c0d83301f81c1e85777172cd6ad2
Zhou Ao <zhouao8@gmail.com>
2014-11-27 00:07:26 +0800

    update css

---
+++ _site/js/height.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	$("#content-inner").css(
+		"padding-bottom",
+		function(index, value) {
+			var h1 = $(document).height();
+			var h2 = $("#sidebar").height();
+			if (h1 - h2 > 100)
+				return (h1 - h2);
+			else return value;
+		}
+	);
+
+	$("#copyright").css({
+		position: 'absolute',
+		bottom: '5px',
+		left: '0px'
+	});;
+
+});
