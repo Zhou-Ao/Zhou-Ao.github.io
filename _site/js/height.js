4981b1ddd05e990979d3bc1a9b47c15fb2820fa8
Zhou Ao <zhouao8@gmail.com>
2014-11-27 00:57:06 +0800

    test mobile

--- _site/js/height.js
+++ _site/js/height.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-	$("#content-inner").css(
-		"padding-bottom",
-		function(index, value) {
-			var h1 = $(document).height();
-			var h2 = $("#sidebar").height();
-			if (h1 - h2 > 100)
-				return (h1 - h2);
-			else return value;
-		}
-	);
+// 	$("#content-inner").css(
+// 		"padding-bottom",
+// 		function(index, value) {
+// 			var h1 = $(document).height();
+// 			var h2 = $("#sidebar").height();
+// 			if (h1 - h2 > 100)
+// 				return (h1 - h2);
+// 			else return value;
+// 		}
+// 	);
 
-	$("#copyright").css({
-		position: 'absolute',
-		bottom: '5px',
-		left: '0px'
-	});;
+// 	$("#copyright").css({
+// 		position: 'absolute',
+// 		bottom: '5px',
+// 		left: '0px'
+// 	});;
 
-});
+// });
